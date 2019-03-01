d377ba894c2ec157216b3fbab426677ac43efe20
Andrey Bakhvalov <bakhvalov.andrey@gmail.com>
2019-03-01 18:05:57 +0300

    docs: ✏️ add basic usage docs

--- styleguide.config.js
+++ styleguide.config.js
@@ -2,7 +2,25 @@ const path = require('path');
 const webpack = require('webpack');
 
 module.exports = {
+  title: 'Urbica React Map GL Draw',
+  moduleAliases: {
+    '@urbica/react-map-gl-draw': path.resolve(__dirname, 'src')
+  },
   require: [path.resolve(__dirname, 'styleguide.setup.js')],
+  sections: [
+    {
+      name: 'Examples',
+      components: () => [
+        'docs/basic-usage.md'
+      ],
+      exampleMode: 'expand'
+    },
+    {
+      name: 'Props',
+      components: 'src/components/**/index.js',
+      usageMode: 'expand'
+    }
+  ],
   dangerouslyUpdateWebpackConfig: (webpackConfig) => {
     const envPlugin = new webpack.EnvironmentPlugin(['MAPBOX_ACCESS_TOKEN']);
     webpackConfig.plugins.push(envPlugin);
